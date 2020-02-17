857eeae3e0aed92cf0de6498c5187ebaef95e406
Marcio Fernandes Dos Santos <farciomernandes@gmail.com>
2020-02-17 12:06:17 -0300

    Lupa de Pesquisa e Aprimoramento
    Edição nos arquivos JSX.
    Ainda sem realizar a busca no backand corretamente!

--- src/components/busca.jsx
+++ src/components/busca.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Title from './Title';
 import api from '../services/api';
+import { Link } from 'react-router-dom';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, Button, Alert
+} from 'reactstrap';
+
 //PESQUISAR COMO USAR API E FILTRAR OS FILMES, E IMPRIMIR CORRETAMENTE
-export default function  Filmes ({ genre }){
+export default function  Filmes ({ e }){
         const [ pesquisa, setPesquisa ] = useState([]);
 
         useEffect(()=> {
@@ -10,12 +16,32 @@ export default function  Filmes ({ genre }){
             const response = await api.get('/movies');
             
             const filter = response.data.filter(busca => {
-                    return busca.genre === genre;
+                    return busca.e === e;
             });
 
             setPesquisa(filter);
         };
         BuscandoFilmes();
-        },[genre]);
-
+        },[e]);
+        
+        const resultadoPesquisa = movie => {
+            return(
+                <Card key={pesquisa._id} className='card'>
+                    <CardImg top width="100%" className='imgCard' src={pesquisa.poster_url} alt="movie poster" />
+                    <CardBody>
+                        <CardTitle className='cardTitle'>{pesquisa.title}</CardTitle>
+                        <CardText>{pesquisa.sinopse}</CardText>
+                        <Button tag={Link} to={`movies/info/${pesquisa._id}`} className='btn btn-danger'>Mais Informações</Button>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return(
+            <div className='container moviesContainer'>
+                <Title title={e} />
+                {
+                    pesquisa.map(pesquisa => resultadoPesquisa(pesquisa))
+                }
+            </div>
+            )
     };
--- src/components/pesquisa.jsx
+++ src/components/pesquisa.jsx
@@ -1,8 +1,6 @@
 import React, { Component, useState, useEffect, Fragment } from 'react';
 import chama from './busca';
-import { render } from '@testing-library/react';
-
-import Title from './Title';
+import lupa from './lupa.jpg'
 export default class Buscando extends Component{
     
     
@@ -11,14 +9,15 @@ export default class Buscando extends Component{
         busca:"Procurar "
     };
 
-    setBusca(e){
+    setBusca(e) {
         this.setState({busca:e.target.value})
     }
     render(){
     return(
         <Fragment>
-        <input type="text" placeholder="Buscar...."
-        value={this.busca} onchange={e=>this.setBusca(e)}/>
+        <input type="text" placeholder="Buscar..."
+        value={this.busca} onchange={e=>this.setBusca(e)}/>  
+         <img src={lupa} alt="Lupa" width="40px" onClick={e=>chama(e)}/>
         </Fragment>
     )};
 };
